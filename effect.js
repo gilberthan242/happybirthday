820564a1b9eaf00b564c1b9aab63febcd2138591
gilberthan242 <78791599+gilberthan242@users.noreply.github.com>
2023-11-07 22:50:29 +0700

    Update effect.js

--- effect.js
+++ effect.js
@@ -162,13 +162,13 @@ $('document').ready(function(){
 		$('#b7').attr('id','b77')
 		$('#b8').attr('id','b88')
 		$('#b9').attr('id','b99')
-		$('#b11').animate({top:240, left: vw-400},500);
-		$('#b22').animate({top:240, left: vw-300},500);
-		$('#b33').animate({top:240, left: vw-200},500);
-		$('#b44').animate({top:240, left: vw-100},500);
-		$('#b55').animate({top:240, left: vw-50},500);
-		$('#b66').animate({top:240, left: vw+50},500);
-		$('#b77').animate({top:240, left: vw+100},500);
+		$('#b11').animate({top:240, left: vw-200},500);
+		$('#b22').animate({top:240, left: vw-150},500);
+		$('#b33').animate({top:240, left: vw-100},500);
+		$('#b44').animate({top:240, left: vw+10},500);
+		$('#b55').animate({top:240, left: vw+50},500);
+		$('#b66').animate({top:240, left: vw+100},500);
+		$('#b77').animate({top:240, left: vw+150},500);
 		$('#b88').animate({top:240, left: vw+200},500);
 		$('#b99').animate({top:240, left: vw+300},500);
 		$('#b1010').animate({top:240, left: vw+400},500);
